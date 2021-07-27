6596f204fd16fa2dbb43550b7456fbc6e5d0773a
Dagmar Levstek <levstekova.dagmar@gmail.com>
2021-07-27 22:18:49 +0200

    secondCommit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function compute()
 {
-    // p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
@@ -26,4 +25,3 @@ function negativeNumber()
    return false;
   }
 }
-        
